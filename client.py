1768b1d758deeebb6de512ccdc18bc4f158d7382
Abhishek Patel <abhi.p@rutgers.edu>
2020-02-29 16:38:32 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import socket
 import sys
 
-#* .AF_INET corresponds to IPV4
-#* SOCK_STREAM corresponds to TCP
+
+# * .AF_INET corresponds to IPV4
+# * SOCK_STREAM corresponds to TCP
 
 # def print2DArray(array):
 #     for row in array:
@@ -18,18 +19,20 @@ def convertFileToArray(file_path):
     result = []
 
     contents = f.readlines()
-    for line in contents:        
+    for line in contents:
         line = line.rstrip('\n')
         line = line.rstrip('\r')
 
         # split line on space
-        splitString = line.split()        
+        splitString = line.split()
 
         result.append(splitString)
-                
-    return result  
+
+    return result
+
 
 def client():
+    f = open("RESOLVED.TXT", 'w')
     file_path = sys.argv[1] + ""
 
     rs_listen_port = int(sys.argv[2])
@@ -38,21 +41,31 @@ def client():
 
     host_names = convertFileToArray(file_path)
 
-    for user_domain in host_names:    
-        user_domain = user_domain[0]    
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # print("[C]: Client socket created")
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # print("[C]: Client socket created")
 
-        except s.error as err:
-            print('{} \n'.format("socket open error ",err))
-        
-        # pass in IP and port you want to connect to
-        connection = (socket.gethostbyname(socket.gethostname()), rs_listen_port)
-        s.connect(connection)
-        
+    except s.error as err:
+        print('{} \n'.format("socket open error ", err))
+
+    connection = (socket.gethostbyname(socket.gethostname()), rs_listen_port)
+    s.connect(connection)
+
+    # Connect to TS server
+    try:
+        ts = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # print("[C]: Client socket created")
+    except ts.error as err:
+        print('{} \n'.format("socket open error ", err))
+
+    connection = (socket.gethostbyname(socket.gethostname()), ts_listen_port)
+    ts.connect(connection)
+
+    for user_domain in host_names:
+        user_domain = user_domain[0]
+
         # sending user word to server
-        # s.send(word.encode('utf-8')) 
+        # s.send(word.encode('utf-8'))
         s.send(user_domain.encode('utf-8'))
 
         # recieving converted word from server
@@ -60,37 +73,21 @@ def client():
 
         decodedWord = rs_server_response.decode('utf-8')
 
-        splitString = decodedWord.split()        
+        splitString = decodedWord.split()
 
-        if(splitString[2] == "NS"):
-            # Connect to TS server        
-            try:
-                ts = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                # print("[C]: Client socket created")
-            except ts.error as err:
-                print('{} \n'.format("socket open error ",err))   
-
-            connection = (socket.gethostbyname(socket.gethostname()), ts_listen_port)
-            ts.connect(connection)
-
+        if splitString[2] == "NS":
             ts.send(user_domain.encode('utf-8'))
 
             ts_server_response = ts.recv(100)
 
             decodedWord = ts_server_response.decode('utf-8')
-            
-            f = open("RESOLVED.TXT", 'a+')
-            f.write(decodedWord + '\n')
-            f.close()
-        else:            
-            f = open("RESOLVED.TXT", 'a+')
-            f.write(decodedWord + '\n')
-            f.close()
-        
+        f.write(decodedWord + '\n')
 
-        s.close()
-    
+    s.close()
+    ts.close()
+    f.close()
     exit()
+
 
 client()
 
